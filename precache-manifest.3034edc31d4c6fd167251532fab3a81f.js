6a1838b6bbd626ae908ab8083af06890edf7c078
iamparthvaghela <parthvgla@gmail.com>
2023-03-25 21:56:35 +0530

    Updates

---
+++ precache-manifest.3034edc31d4c6fd167251532fab3a81f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f109997bd105e93110d",
+    "url": "/static/css/main.90e23912.chunk.css"
+  },
+  {
+    "revision": "1f109997bd105e93110d",
+    "url": "/static/js/main.1f109997.chunk.js"
+  },
+  {
+    "revision": "c0dba26f4ccf71cea450",
+    "url": "/static/js/1.c0dba26f.chunk.js"
+  },
+  {
+    "revision": "20e69ca38c52ec4e772f",
+    "url": "/static/js/2.20e69ca3.chunk.js"
+  },
+  {
+    "revision": "561708c8e89dc260303a",
+    "url": "/static/js/runtime~main.561708c8.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "0b4df9e2778410c03d2dd6a6e3c203da",
+    "url": "/index.html"
+  }
+];
